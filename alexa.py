3b855eac880b893b67748235a992f2d519eee598
jaskeerat singh <jaskee789@gmail.com>
2020-12-01 01:20:56 +0530

    alexa account linking

--- alexa.py
+++ alexa.py
@@ -3,7 +3,13 @@ from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
-from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model.ui import SimpleCard, LinkAccountCard
+import requests
+
+
+def get_html(url):
+    print(requests.get(url).json())
+    return requests.get(url).json()
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
@@ -13,12 +19,28 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
 
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World",
-                       speech_text)).set_should_end_session(False)
-        return handler_input.response_builder.response
+        access_token = handler_input.request_envelope.context.system.user.access_token
+        if (access_token == None):
+            speech_text = "Please use Alexa companion app to authenticate"
+            # speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+            handler_input.response_builder.speak(speech_text).set_card(
+                LinkAccountCard()).set_should_end_session(False)
+            return handler_input.response_builder.response
+
+        else:
+
+            url = "https://api.amazon.com/user/profile?access_token=" + access_token
+            speech_text = "This is the default message."
+            data = get_html(url)
+            speech_text = f"Hi {data['name']}. I have your email address as: {data['email']}"
+            print(speech_text)
+            # speech_text = " got your data"
+            handler_input.response_builder.speak(speech_text).set_card(
+                SimpleCard("Got data",
+                           speech_text)).set_should_end_session(False)
+            return handler_input.response_builder.response
 
 
 class HelloWorldIntentHandler(AbstractRequestHandler):
--- run.py
+++ run.py
@@ -188,4 +188,4 @@ def logout(username):
 
 if __name__ == "__main__":
     # create_db()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
